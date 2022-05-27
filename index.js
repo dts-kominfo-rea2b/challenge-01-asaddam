3cafe406c076e559ad5afdea75e5de7f44c4779c
user <user@ROBOT.local>
2022-05-27 09:55:54 +0700

    done

--- index.js
+++ index.js
@@ -1,11 +1,74 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+let firstFavColor = new Set(["Yellow", "Pink", "White", "Purple"]);
+let firstFavResto = new Set([
+    "Bento",
+    "Sushi",
+    "Pancake",
+    "Eggy",
+    "Tempura",
+    "Bento",
+    "Eggy",
+    "Padang",
+    "Tteok",
+    "Sushi",
+    "Sushi",
+]); 
+const firstUser = [{
+    name: "Monica",
+    gender: "Female",
+    age: 17,
+    email: "monica@dingdong.com",
+    favoriteColor: [...firstFavColor],
+    isHavePet: "Yes",
+    education: [{
+        name: "SD 01",
+        city: "Jakarta",
+        graduate: "2016",
+    }, {
+        name: "SMP 02",
+        city: "Jakarta",
+        graduate: "2019",
+    }, {
+        name: "SMA 03",
+        city: "Tangerang",
+    }],
+    favoriteRestaurant: [...firstFavResto],
+}];
+
+let secondFavColor = new Set(["Blue", "Black", "Grey"]);
+let secondFavResto = new Set([
+    "Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"
+]);
+const secondUser = [{
+    name: "Wendy",
+    gender: "Male",
+    age: 23,
+    email: "wendy@dingdong.com",
+    favoriteColor: [...secondFavColor],
+    isHavePet: "No",
+    education: [{
+        name: "SD 02",
+        city: "Jakarta",
+        graduate: "2010",
+    }, {
+        name: "SMP 03",
+        city: "Bogor",
+        graduate: "2013",
+    }, {
+        name: "SMA 01",
+        city: "Surabaya",
+        graduate: "2016",
+    }, {
+        name: "Universitas Maju",
+        city: "Tangerang",
+    }],
+    favoriteRestaurant: [...secondFavResto],
+}];
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = firstUser.concat(secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
@@ -16,5 +79,5 @@ function main() {
 main();
 
 module.exports = {
-    users
+    users,
 };
